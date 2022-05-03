a0fc81e583b1255be80f826debf58a841d74ddc2
hahaocen <103203631+hahaocen@users.noreply.github.com>
2022-05-03 16:39:36 -0700

    Update skilldemo2test.java

--- skilldemo2test.java
+++ skilldemo2test.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class skilldemo2test {
     @Test
     public void addition() {
-        assertEquals(2,skilldemo2.addition(2, 0));
+        assertEquals(2,skilldemo2.addition(2, 1));
     }
 }
